c055382bbcf5bf6e818130781991e1a1d3956e1b
Kristina Martinez <kristinamartinez@Kristinas-MacBook-Pro.local>
2018-03-19 10:58:48 -0400

    temperature conversion test

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,10 @@
 #write your code here
+def ftoc(temp)
+  converted_temp = ((temp - 32) * (0.555555556)).to_i
+  return converted_temp
+end
+
+def ctof(temp)
+  converted_temp = ((temp * 1.8) + 32)
+  return converted_temp
+end
